19b5dfc0d12618ec319f2cd1647033a5bf2c318b
Shane Mendez <mendezsoftware@gmail.com>
2022-07-21 21:49:25 -0500

    changes

--- tmux-install.sh
+++ tmux-install.sh
@@ -7,11 +7,12 @@ fi
 
 if [ -f ~/.tmux.conf ] 
 then 
-	cp ~/.tmux.conf ./.tmux.conf.old
+	mv  ~/.tmux.conf ./.tmux.conf.old
 	echo "Moved old tmux conf here as .tmux.conf.old"
+
 fi
 
-mv tmux.conf ~/.tmux.conf
+cp  tmux.conf ~/.tmux.conf
 echo "Copied tmux.conf to ~/.tmux.conf"
 tmux source ~/.tmux.conf
 echo "Done"
